d8a16022e8debf4f4b8cbfc3183a7e06639d887b
Nate <natepwolf@gmail.com>
2024-11-11 14:20:06 -0500

    created User repo test file

---
+++ server/src/test/java/learn/fitness/data/UserJdbcTemplateRepositoryTest.java
@@ -0,0 +1,16 @@
+package learn.fitness.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class UserJdbcTemplateRepositoryTest {
+
+    @Autowired
+    UserJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState state;
+}
