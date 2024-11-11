2ce3a84d70326f433966b70378c6589d39909118
Yves Paultre <paultrepack1@gmail.com>
2024-11-11 14:14:02 -0500

    Updated application.properties files; created KnownGoodState for state management in tests; began work on tests

--- server/src/test/java/learn/fitness/data/WorkoutJdbcTemplateRepositoryTest.java
+++ server/src/test/java/learn/fitness/data/WorkoutJdbcTemplateRepositoryTest.java
@@ -1,7 +1,18 @@
 package learn.fitness.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class WorkoutJdbcTemplateRepositoryTest {
 
+    final static int NEXT_ID = 10;
+
+    @Autowired
+    WorkoutJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState state;
 }
