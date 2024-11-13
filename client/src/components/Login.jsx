214b05a12d3c76ab46bd7d5cce2b15555b2c861c
bphan23 <bphan23@uw.edu>
2024-11-13 13:50:08 -0800

    fixed bugs in logout and user context component.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,86 +1,101 @@
-import { createContext, useState } from "react"
-import { Container, Form, Button, ListGroup, ListGroupItem } from "react-bootstrap"
-import { User } from "../context/UserContext"
+import { createContext, useState } from "react";
+import {
+  Container,
+  Form,
+  Button,
+  ListGroup,
+  ListGroupItem,
+  Alert,
+} from "react-bootstrap";
+import User from "../context/UserContext";
 
 const USER_DEFAULT = {
-    username: '',
-    password: ''
-}
+  username: "",
+  password: "",
+};
 
-const Login = ()=>{
-    // TODO: Add Error Handling
-    const [credentials, setCredentials] = useState(USER_DEFAULT);
-    const [errors, setErrors] = useState([]);
-    const url = 'http://localhost:8080/api/user/authenticate'
-    
-    const handleSubmit = (event)=>{
-        event.preventDefault();
-        
-        const init = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials)
+const Login = () => {
+  // TODO: Add Error Handling
+  const [credentials, setCredentials] = useState(USER_DEFAULT);
+  const [errors, setErrors] = useState([]);
+  const url = "http://localhost:8080/api/user/authenticate";
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const init = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    };
+
+    fetch(url, init)
+      .then((response) => {
+        if (response.status === 200 || response.status === 403) {
+          return response.json();
+        } else {
+          return Promise.reject(`Unexpected status code: ${response.status}`);
         }
+      })
+      .then((data) => {
+        if (data.jwt_token) {
+          User = data.jwt_token;
+        } else {
+          setErrors(data);
+        }
+      })
+      .catch(console.log);
+  };
 
-        fetch(url, init)
-        .then(response => {
-            if(response.status === 200 || response.status === 403){
-                return response.json();
-            } else {
-                return Promise.reject(`Unexpected status code: ${response.status}`)
-            }
-        }).then(data => {
-            if(data.jwt_token){
-                User = data.jwt_token;
-            } else {
-                setErrors(data)
-            }
-        }).catch(console.log);
-    }
+  const handleChange = (event) => {
+    const newCredentials = { ...credentials };
+    newCredentials[event.target.name] = event.target.value;
+    setCredentials(newCredentials);
+  };
 
-    const handleChange = (event) =>{
-        const newCredentials = {...credentials};
-        newCredentials[event.target.name] = event.target.value;
-        setCredentials(newCredentials);
-    }
+  const renderErrors = (err) => {};
 
-    const renderErrors = (err) =>{
+  return (
+    <Container>
+      <h2 className="login-title">Log In</h2>
+      {errors.length > 0 && (
+        <Alert key={errors}>
+          <p>The following errors were found:</p>
+          <ListGroup>
+            {errors.map((error) => (
+              <ListGroupItem key={error}>{error}</ListGroupItem>
+            ))}
+          </ListGroup>
+        </Alert>
+      )}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group
+          className="mb-3"
+          controlId="username"
+          onChange={handleChange}
+        >
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group
+          className="mb-3"
+          controlId="password"
+          onChange={handleChange}
+        >
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Log In
+        </Button>
+        <Button variant="secondary" href="/register">
+          Register
+        </Button>
+      </Form>
+    </Container>
+  );
+};
 
-    }
-
-    return (
-        <Container>
-            <h2 className="login-title">Log In</h2>
-            {errors.length > 0 && (
-                <Alert key={errors}>
-                    <p>The following errors were found:</p>
-                    <ListGroup>
-                        {errors.map(error => (
-                            <ListGroupItem key={error}>{error}</ListGroupItem>
-                        ))}
-                    </ListGroup>
-                </Alert>
-            )}
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className='mb-3' controlId="username" onChange={handleChange}>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text"/>
-                </Form.Group>
-                <Form.Group className='mb-3' controlId="password" onChange={handleChange}>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Log In
-                </Button>
-                <Button variant="secondary" href="/register">
-                    Register
-                </Button>
-            </Form>
-        </Container>
-    )
-}
-
-export default Login
+export default Login;
--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,7 +1,6 @@
 import { createContext } from "react";
 
-
 // Update with authentication token from authenticate path
-const User = createContext(null)
+const User = createContext(null);
 
-export default User
+export default User;
